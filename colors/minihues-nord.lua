3afb74d11ed59317cefa5b2e55a632c66187c5a2
Pete Kazmier <pete@kazmier.com>
2024-05-07 13:10:00 -0400

    feat: add minihues nord theme

---
+++ colors/minihues-nord.lua
@@ -0,0 +1,9 @@
+local hues = require("mini.hues")
+vim.g.colors_name = "minihues-nord"
+local opts = {
+  accent = "azure",
+  background = "#1c2231",
+  foreground = "#c4c7cd",
+}
+hues.setup(opts)
+require("plugins.mini.hues").apply_custom_highlights(opts)
