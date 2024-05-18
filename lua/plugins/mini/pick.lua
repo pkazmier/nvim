40a33ce11004edb9bf35fb24b386f2287fd9ae12
Pete Kazmier <pete@kazmier.com>
2024-05-18 17:38:43 -0400

    feat: add a colorscheme picker

--- lua/plugins/mini/pick.lua
+++ lua/plugins/mini/pick.lua
@@ -11,3 +11,15 @@ vim.ui.select = MiniPick.ui_select
 MiniPick.registry.config = function()
   return MiniPick.builtin.files(nil, { source = { cwd = vim.fn.stdpath("config") } })
 end
+
+MiniPick.registry.colorschemes = function()
+  local colorschemes = vim.fn.getcompletion("", "color")
+  return MiniPick.start({
+    source = {
+      items = colorschemes,
+      choose = function(item)
+        vim.cmd("colorscheme " .. item)
+      end,
+    },
+  })
+end
