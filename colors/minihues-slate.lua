fa98a36da4f38cb7799f30c306e5cfe934f6e8ca
Pete Kazmier <pete@kazmier.com>
2024-05-19 11:18:25 -0400

    feat: a better slate colorscheme

--- colors/minihues-slate.lua
+++ colors/minihues-slate.lua
@@ -2,8 +2,8 @@ local hues = require("mini.hues")
 vim.g.colors_name = "minihues-slate"
 local opts = {
   accent = "azure",
-  background = "#1c2231",
-  foreground = "#c4c7cd",
+  background = "#1a2331",
+  foreground = "#c3c7cd",
 }
 hues.setup(opts)
 require("plugins.mini.hues").apply_custom_highlights(opts)
