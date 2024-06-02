abc4fec660023ece1ce48c7f951dc42e83d60b5a
Pete Kazmier <pete@kazmier.com>
2024-06-02 09:46:32 -0400

    feat: add <leader>mT to toggle MiniMap per buffer

--- lua/plugins/mini/map.lua
+++ lua/plugins/mini/map.lua
@@ -1,13 +1,17 @@
+local M = {}
+local H = {}
 local map = require("mini.map")
 
--- stylua: ignore start
-local enable = {
+-- stylua: ignore
+M.auto_enable = {
   go       = true,
   lua      = true,
   markdown = true,
   python   = true,
   rust     = true,
 }
+
+-- stylua: ignore
 map.setup({
   integrations = {
     map.gen_integration.builtin_search(),
@@ -27,24 +31,53 @@ map.setup({
     zindex = 21,
   },
 })
--- stylua: ignore end
 
 for _, key in ipairs({ "n", "N", "*" }) do
   vim.keymap.set("n", key, key .. "zv<Cmd>lua MiniMap.refresh({}, { lines = false, scrollbar = false })<CR>")
 end
 
+-- Return true if the current buffer is supposed to have a map.
+-- 1. User has expilicity enabled it via M.buf_toggle
+-- 2. Filetype of buffer is in the M.auto_enable table
+H.should_be_enabled = function()
+  local ft = vim.bo.filetype
+  local disabled = vim.b.minimap_disable
+  local enabled_explicitly = vim.b.minimap_disable == false
+  return enabled_explicitly or M.auto_enable[ft] and not disabled
+end
+
+-- Toggle the global visibility of the map. If it is currently shown, then
+-- hide it. If it is not, then show it if the current buffer is supposed to
+-- have a map.
+M.toggle = function()
+  vim.g.minimap_disable = not vim.g.minimap_disable
+  if H.should_be_enabled() then
+    MiniMap.toggle()
+  end
+end
+
+-- Toggle whether the current buffer should display a map if it has not been
+-- globally disabled via M.toggle.
+M.buf_toggle = function()
+  if H.should_be_enabled() then
+    vim.b.minimap_disable = true
+    MiniMap.close()
+  else
+    vim.b.minimap_disable = false
+    MiniMap.open()
+  end
+end
+
 vim.api.nvim_create_autocmd("BufEnter", {
   group = vim.api.nvim_create_augroup("kaz-minimap", { clear = true }),
   desc = "Toggle 'mini.map' based on filetype",
-  callback = function(data)
-    if vim.bo[data.buf].buftype ~= "" then
-      return
-    end
-    local ft = vim.bo[data.buf].filetype
-    if enable[ft] then
+  callback = function()
+    if H.should_be_enabled() then
       MiniMap.open()
     else
       MiniMap.close()
     end
   end,
 })
+
+return M
