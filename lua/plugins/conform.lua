f7bb379b6e87dfae9cf68d62a81c5274d2dd8293
Pete Kazmier <pete@kazmier.com>
2024-06-19 09:00:32 -0400

    feat: use conform's new lsp_format option

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,19 +1,23 @@
+local no_lsp_fallback = {
+  c = true,
+  cpp = true,
+}
 require("conform").setup({
   notify_on_error = true,
   format_after_save = function(bufnr)
     if vim.g.disable_autoformat then
       return
     end
-    local disable_filetypes = { c = true, cpp = true }
+    local filetype = vim.bo[bufnr].filetype
     return {
       timeout_ms = 5000,
-      lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+      lsp_format = no_lsp_fallback[filetype] and "never" or "fallback",
     }
   end,
   formatters_by_ft = {
     lua = { "stylua" },
     -- Conform can also run multiple formatters sequentially
-    python = { "isort", "black" },
+    python = { "isort", "black" }, -- ruff doesn't support sort on format ... sigh
     -- Run *until* a formatter is found.
     markdown = { { "prettierd", "prettier" } },
     javascript = { { "prettierd", "prettier" } },
