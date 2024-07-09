8522a6603c2e48a81fd0d83f28b8c0daa92d62b3
Pete Kazmier <pete@kazmier.com>
2024-07-09 16:49:50 -0400

    fix: tweak go and python filetype autocommands

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -18,19 +18,30 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 })
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
+  group = vim.api.nvim_create_augroup("kaz-python-settings", { clear = true }),
+  pattern = { "python" },
+  desc = "Local settings for Python buffers",
+  callback = function()
+    vim.b.miniindentscope_config = { options = { border = "top" } }
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  group = vim.api.nvim_create_augroup("kaz-golang-settings", { clear = true }),
+  pattern = { "go" },
+  desc = "Local settings for Go buffers",
+  callback = function()
+    vim.bo.tabstop = 8
+    vim.bo.shiftwidth = 0
+    vim.bo.expandtab = false
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
   group = vim.api.nvim_create_augroup("kaz-disable-minianimate", { clear = true }),
   pattern = { "minifiles" },
   desc = "Disable animate in minifiles (preview goes bonkers)",
   callback = function()
     vim.b.minianimate_disable = true
-  end,
-})
-
-vim.api.nvim_create_autocmd({ "FileType" }, {
-  group = vim.api.nvim_create_augroup("kaz-enable-minihipatterns", { clear = true }),
-  pattern = { "minideps-confirm" },
-  desc = "Enable MiniHipatterns in MiniDeps confirm buffers",
-  callback = function()
-    MiniHipatterns.enable()
   end,
 })
