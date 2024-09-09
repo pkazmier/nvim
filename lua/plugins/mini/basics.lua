039d31107edd5655b2aa847aa0621d391c3287a0
Pete Kazmier <pete@kazmier.com>
2024-09-08 21:15:32 -0400

    docs: note about why we disable transparency

--- lua/plugins/mini/basics.lua
+++ lua/plugins/mini/basics.lua
@@ -1,5 +1,9 @@
 require("mini.basics").setup({
   options = {
+    -- NOTE: This is disable intentionally.
+    -- When enabled, it results in small icons within MiniFiles floating
+    -- windows. This has something to do with the winblend (transparency)
+    -- setting. So, it's a tradeoff beteween transparency or bigger icons.
     extra_ui = false,
   },
   mappings = {
