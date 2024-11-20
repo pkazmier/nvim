09f4502ae2560040394a1bb939552780974d9257
Pete Kazmier <pete@kazmier.com>
2024-11-20 08:21:19 -0500

    feat: use h/l to navigate in oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,6 +1,8 @@
 local oil = require("oil")
 oil.setup({
   keymaps = {
+    ["h"] = "actions.parent",
+    ["l"] = "actions.select",
     ["<C-h>"] = false,
     ["<C-l>"] = false,
     ["<C-s>"] = false,
