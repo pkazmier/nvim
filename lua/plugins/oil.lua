4c4d03327dcf1af93e0d25b079b1848531203947
Pete Kazmier <pete@kazmier.com>
2024-11-23 08:48:50 -0500

    fix: disable folding by expr in oil windows
    See discussion here:
    https://github.com/stevearc/oil.nvim/pull/467#issuecomment-248895275o6

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,5 +1,9 @@
 local oil = require("oil")
 oil.setup({
+  win_options = {
+    foldenable = false,
+    foldmethod = "manual",
+  },
   keymaps = {
     ["h"] = "actions.parent",
     ["l"] = "actions.select",
