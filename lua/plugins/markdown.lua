2d4479af459d4b1bf787114a9a7c23763bff34cd
Pete Kazmier <pete@kazmier.com>
2024-12-20 15:45:48 -0500

    feat: add markdown config for better headlines

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,22 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  opts = {
+    code = {
+      sign = false,
+      position = "right",
+      width = "block",
+      left_pad = 1,
+      right_pad = 1,
+    },
+    heading = {
+      sign = false,
+      border = true,
+      width = "block",
+      below = "▔",
+      above = "▁",
+      left_pad = 2,
+      right_pad = 4,
+      icons = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " },
+    },
+  },
+}
