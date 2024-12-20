e1e65608296ea38f53c203d4562b235d756f2650
Pete Kazmier <pete@kazmier.com>
2024-12-20 15:42:10 -0500

    feat: slants for lualine and bufferline
    I prefer the slants over the powerline symbols in lualine.

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,8 +1,17 @@
 return {
+  { "akinsho/bufferline.nvim", opts = { options = { separator_style = "slope" } } },
   {
     "folke/tokyonight.nvim",
     opts = function(_, opts)
       opts.lualine_bold = true
+      return opts
+    end,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      opts.options.section_separators = { left = "", right = "" }
+      opts.options.component_separators = { left = "╲", right = "╱" }
       return opts
     end,
   },
