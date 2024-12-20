0ff7a71cd01216b5672cd5314e379f6b959cc757
Pete Kazmier <pete@kazmier.com>
2024-12-20 18:27:37 -0500

    feat: single cursorline on screen only

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,24 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Disable cursorline in inactive windows
+local no_cursorlines = { "neo-tree", "trouble", "snacks-dashboard" }
+vim.api.nvim_create_autocmd({ "WinEnter" }, {
+  callback = function()
+    if vim.tbl_contains(no_cursorlines, vim.bo.filetype) then
+      return
+    end
+    vim.opt_local.cursorline = true
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "WinLeave" }, {
+  callback = function()
+    if vim.tbl_contains(no_cursorlines, vim.bo.filetype) then
+      return
+    end
+    vim.opt_local.cursorline = false
+  end,
+})
+
