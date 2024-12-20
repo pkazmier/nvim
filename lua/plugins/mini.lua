3d22c9135e96651feb10e0efd7d7354347858fc5
Pete Kazmier <pete@kazmier.com>
2024-12-20 17:37:23 -0500

    feat: add mini.operators

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -7,4 +7,19 @@ return {
       { "gA", mode = { "n", "v" }, desc = "Align with preview" },
     },
   },
+  {
+    "echasnovski/mini.operators",
+    keys = {
+      { "g=", mode = { "n", "v" }, desc = "Evaluate" },
+      { "gx", mode = { "n", "v" }, desc = "Exchange" },
+      { "gm", mode = { "n", "v" }, desc = "Multiply" },
+      { "gr", mode = { "n", "v" }, desc = "Replace with register" },
+      { "gS", mode = { "n", "v" }, desc = "Sort" },
+    },
+    opts = {
+      sort = {
+        prefix = "gS",
+      },
+    },
+  },
 }
