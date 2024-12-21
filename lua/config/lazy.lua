e999068be7f680b0d8d00dc9e7e01c7105af99fc
Pete Kazmier <pete@kazmier.com>
2024-12-21 10:15:50 -0500

    feat: disable netrw plugin

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -42,7 +42,7 @@ require("lazy").setup({
         "gzip",
         -- "matchit",
         -- "matchparen",
-        -- "netrwPlugin",
+        "netrwPlugin",
         "tarPlugin",
         "tohtml",
         "tutor",
