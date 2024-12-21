ab46cb515c0b4cf0373acc6082ed067cc99f9816
Pete Kazmier <pete@kazmier.com>
2024-12-21 10:16:15 -0500

    feat: customize my dashboard

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -4,6 +4,11 @@ return {
     "folke/tokyonight.nvim",
     opts = function(_, opts)
       opts.lualine_bold = true
+      opts.on_highlights = function(hl, c)
+        hl["SnacksDashboardHeader"] = { fg = c.blue2 }
+        hl["SnacksDashboardTitle"] = { fg = c.magenta, bold = true }
+        hl["SnacksDashboardKey"] = { fg = c.yellow, bold = true }
+      end
       return opts
     end,
   },
@@ -40,8 +45,9 @@ return {
     "snacks.nvim",
     opts = {
       dashboard = {
-        preset = {
-          header = [[
+        width = 40,
+        sections = function()
+          local header = [[
       ████ ██████           █████      ██                    
      ███████████             █████                            
      █████████ ███████████████████ ███   ███████████  
@@ -49,21 +55,33 @@ return {
    █████████ ██████████ █████████ █████ █████ ████ █████  
  ███████████ ███    ███ █████████ █████ █████ ████ █████ 
 ██████  █████████████████████ ████ █████ █████ ████ ██████
- ]],
-        -- stylua: ignore
-        ---@type snacks.dashboard.Item[]
-        keys = {
-          { icon = " ", key = "f", desc = "Find File",       action = ":lua Snacks.dashboard.pick('files')" },
-          { icon = " ", key = "n", desc = "New File",        action = ":ene | startinsert" },
-          { icon = " ", key = "g", desc = "Find Text",       action = ":lua Snacks.dashboard.pick('live_grep')" },
-          { icon = " ", key = "r", desc = "Recent Files",    action = ":lua Snacks.dashboard.pick('oldfiles')" },
-          { icon = " ", key = "c", desc = "Config",          action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
-          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
-          { icon = " ", key = "x", desc = "Lazy Extras",     action = ":LazyExtras" },
-          { icon = "󰒲 ", key = "l", desc = "Lazy",            action = ":Lazy" },
-          { icon = " ", key = "q", desc = "Quit",            action = ":qa" },
-        },
-        },
+]]
+          local function greeting()
+            local hour = tonumber(vim.fn.strftime("%H"))
+            -- [02:00, 10:00) - morning, [10:00, 18:00) - day, [18:00, 02:00) - evening
+            local part_id = math.floor((hour + 6) / 8) + 1
+            local day_part = ({ "evening", "morning", "afternoon", "evening" })[part_id]
+            local username = os.getenv("USER") or os.getenv("USERNAME") or "user"
+            return ("Good %s, %s"):format(day_part, username)
+          end
+          -- stylua: ignore
+          return {
+            { padding = 0, text = { header, hl = "header" } },
+            { align = "center", desc = greeting(), padding = 2 },
+            { title = "Builtin Actions", indent = 2, padding = 1,
+              { icon = " ", key = "f", desc = "Find File",       action = ":lua Snacks.dashboard.pick('files')" },
+              { icon = " ", key = "n", desc = "New File",        action = ":ene | startinsert" },
+              { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+              { icon = " ", key = "q", desc = "Quit",            action = ":qa" } },
+            { title = "Recent Projects", section = "projects", indent = 2, padding = 1 },
+            { title = "Maintenance Actions", indent = 2, padding = 2,
+              { icon = " ", key = "c", desc = "Config",      action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})", },
+              { icon = "󰒲 ", key = "l", desc = "Lazy",        action = ":Lazy" },
+              { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+              { icon = "󱁤 ", key = "m", desc = "Mason",       action = ":Mason" },                          },
+            { section = "startup" },
+          }
+        end,
       },
     },
   },
