86b4ba1912c57fa6f98a22d438dee4b81531f7b7
Pete Kazmier <pete@kazmier.com>
2024-12-21 12:46:21 -0500

    feat: tune markdown render settings

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,22 +1,28 @@
 return {
   "MeanderingProgrammer/render-markdown.nvim",
   opts = {
+    render_modes = { "n", "c", "t", "i" },
+    anti_conceal = {
+      ignore = {
+        -- code_border = true,
+        code_background = true,
+      },
+    },
     code = {
       sign = false,
       position = "right",
-      width = "block",
       left_pad = 1,
       right_pad = 1,
     },
     heading = {
       sign = false,
       border = true,
-      width = "block",
+      -- width = "block",
       below = "▔",
       above = "▁",
-      left_pad = 2,
+      left_pad = 1,
       right_pad = 4,
-      icons = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " },
+      icons = { " ", " ", " ", " ", " ", " " },
     },
   },
 }
