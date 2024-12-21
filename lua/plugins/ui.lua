eb70ad6b78b156cbdf9027c95ed71f7bee378c5c
Pete Kazmier <pete@kazmier.com>
2024-12-21 08:30:23 -0500

    feat: add gap around section a and z in lualine

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -10,6 +10,27 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     opts = function(_, opts)
+      local empty = require("lualine.component"):extend()
+      function empty:draw(default_highlight)
+        self.status = ""
+        self.applied_separator = ""
+        self:apply_highlights(default_highlight)
+        self:apply_section_separators()
+        return self.status
+      end
+
+      local function add_spacer(section, side)
+        local left = side == "left"
+        local separator = left and { right = "" } or { left = "" }
+        section[1] = { section[1], separator = separator }
+        local color = Snacks.util.color("StatusLine", "bg")
+        local spacer = { empty, color = { fg = color, bg = color }, separator = separator }
+        table.insert(section, left and #section + 1 or 1, spacer)
+        return section
+      end
+
+      opts.sections["lualine_a"] = add_spacer(opts.sections["lualine_a"], "left")
+      opts.sections["lualine_z"] = add_spacer(opts.sections["lualine_z"], "right")
       opts.options.section_separators = { left = "", right = "" }
       opts.options.component_separators = { left = "╲", right = "╱" }
       return opts
