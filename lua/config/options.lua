cce1df832091936e31686b667e5eedf8820f6190
Pete Kazmier <pete@kazmier.com>
2024-12-22 11:03:13 -0500

    feat: use basedpyright instead of pyright

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.g.lazyvim_python_lsp = "basedpyright"
+
 vim.opt.fillchars = {
   horiz = "━",
   horizup = "┻",
