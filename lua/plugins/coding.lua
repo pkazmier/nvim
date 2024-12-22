7307288505ed10f002ab3db7e38197e6821de345
Pete Kazmier <pete@kazmier.com>
2024-12-22 09:44:34 -0500

    feat: use C-y to accept completion instead of Enter
    Using Enter to accept completion constantly interferes with my note
    taking in markdown files. At the end of the line, if the completion
    window is open, pressing enter to go to next line will select the
    completion instead. So, using C-y as god intended :-)

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,5 +1,13 @@
 return {
   {
+    "saghen/blink.cmp",
+    opts = {
+      keymap = {
+        preset = "default",
+      },
+    },
+  },
+  {
     "windwp/nvim-autopairs",
     event = "InsertEnter",
     opts = {},
