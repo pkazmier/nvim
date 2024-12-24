47437b72acc07ae353c9ad39ac35dd2b85348467
Pete Kazmier <pete@kazmier.com>
2024-12-23 20:50:53 -0500

    feat: disable copilot completion suggestions

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.g.ai_cmp = false
 vim.g.lazyvim_python_lsp = "basedpyright"
 
 vim.opt.fillchars = {
