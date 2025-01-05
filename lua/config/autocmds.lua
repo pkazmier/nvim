7ad09ceed58aeb67bdbe81dc6c81f199cc6190c2
Pete Kazmier <pete@kazmier.com>
2025-01-05 10:12:12 -0500

    fix: display of virt text above L1 headings in md files

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -27,6 +27,22 @@ vim.api.nvim_create_autocmd({ "WinLeave" }, {
   end,
 })
 
+-- HACK: Show the virtual text above line 1 for L1 headers.
+-- I use the border option on headings in render-markdown, which looks great
+-- except on L1 headers on line 1 because the virtual text above line 1 is
+-- not displayed upon entering the buffer.
+vim.api.nvim_create_autocmd({ "BufReadPost" }, {
+  pattern = "*.md",
+  callback = function()
+    local ctrl_y = vim.api.nvim_replace_termcodes("<c-y>", true, false, true)
+    vim.schedule(function()
+      local current_position = vim.api.nvim_win_get_cursor(0)
+      vim.api.nvim_feedkeys(ctrl_y, "n", true)
+      vim.api.nvim_win_set_cursor(0, current_position)
+    end)
+  end,
+})
+
 -- Set buffer local options based on filetype
 local filetype_options = {
   go = {
