1e0aa3a6f8a445f4eabab1e1d5646ad0c6317621
Pete Kazmier <pete@kazmier.com>
2025-01-25 14:11:21 -0500

    refactor: use folke's auto cursorline autocmds

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -8,22 +8,20 @@
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
 -- Disable cursorline in inactive windows
-local no_cursorlines = { "neo-tree", "trouble", "snacks_dashboard", "snacks_picker_input" }
-vim.api.nvim_create_autocmd({ "WinEnter" }, {
+vim.api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
   callback = function()
-    if vim.tbl_contains(no_cursorlines, vim.bo.filetype) then
-      return
+    if vim.w.auto_cursorline then
+      vim.wo.cursorline = true
+      vim.w.auto_cursorline = nil
     end
-    vim.opt_local.cursorline = true
   end,
 })
-
-vim.api.nvim_create_autocmd({ "WinLeave" }, {
+vim.api.nvim_create_autocmd({ "InsertEnter", "WinLeave" }, {
   callback = function()
-    if vim.tbl_contains(no_cursorlines, vim.bo.filetype) then
-      return
+    if vim.wo.cursorline then
+      vim.w.auto_cursorline = true
+      vim.wo.cursorline = false
     end
-    vim.opt_local.cursorline = false
   end,
 })
 
