24e72a07d5b65e91fde104931939af117cec2a3f
Pete Kazmier <pete@kazmier.com>
2025-02-09 15:33:35 -0500

    chore: remove unused fzf-lua config
    We've move to snacks picker now.

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -16,26 +16,6 @@ return {
     },
   },
   {
-    "ibhagwan/fzf-lua",
-    keys = {
-      {
-        "<leader>fp",
-        LazyVim.pick("files", { cwd = require("lazy.core.config").options.root }),
-        desc = "Find Plugin File",
-      },
-    },
-    opts = function(_, opts)
-      if vim.g.kaz_transparency then
-        opts.fzf_colors = {
-          true,
-          bg = "-1",
-          gutter = "-1",
-        }
-      end
-      return opts
-    end,
-  },
-  {
     "nvzone/typr",
     dependencies = "nvzone/volt",
     opts = {},
