b5f14968b6c4f5c593f0d41706320d158f11619b
Pete Kazmier <pete@kazmier.com>
2025-09-01 12:33:58 -0400

    fix: mini.nvim was moved to github.com/nvim-mini

--- init.lua
+++ init.lua
@@ -1,16 +1,15 @@
 --- Bootstrap 'mini.nvim' manually in a way that it gets managed by 'mini.deps'
-local mini_path = vim.fn.stdpath('data') .. '/site/pack/deps/start/mini.nvim'
+local mini_path = vim.fn.stdpath("data") .. "/site/pack/deps/start/mini.nvim"
 if not vim.loop.fs_stat(mini_path) then
   vim.cmd('echo "Installing `mini.nvim`" | redraw')
-  local clone_cmd = { 'git', 'clone', '--filter=blob:none', 'https://github.com/echasnovski/mini.nvim', mini_path }
+  local clone_cmd = { "git", "clone", "--filter=blob:none", "https://github.com/nvim-mini/mini.nvim", mini_path }
   vim.fn.system(clone_cmd)
-  vim.cmd('packadd mini.nvim | helptags ALL')
+  vim.cmd("packadd mini.nvim | helptags ALL")
   vim.cmd('echo "Installed `mini.nvim`" | redraw')
 end
 
 -- Set up 'mini.deps' immediately to have its `now()` and `later()` helpers
-require('mini.deps').setup()
+require("mini.deps").setup()
 
 -- Define main config table to be able to use it in scripts
 _G.Config = {}
-
