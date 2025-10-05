1e59aefa174be6f5041c96df1d1b36a6067aa0ad
Pete Kazmier <pete@kazmier.com>
2025-10-05 08:31:54 -0400

    feat(minihues): add new purple minihues theme (beautiful IMO)

--- colors/minihues-darkpurple.lua
+++ colors/minihues-darkpurple.lua
@@ -1,11 +1,11 @@
 local hues = require("mini.hues")
 local opts = {
   accent = "purple",
-  background = "#232030",
-  foreground = "#c6c6cd",
+  background = "#151025",
+  foreground = "#B6BCBF",
 }
 hues.setup(opts)
 local p = hues.get_palette()
 p.accent_bg = p.bg_edge
 hues.apply_palette(p)
-vim.g.colors_name = "minihues-purple"
+vim.g.colors_name = "minihues-darkpurple"
