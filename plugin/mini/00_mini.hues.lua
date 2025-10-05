e3f31294cb29168063b13dd2db3813db4e7b2132
Pete Kazmier <pete@kazmier.com>
2025-10-05 08:06:06 -0400

    fix(minihues): use MiniJump2dDim for inactive starter items

--- plugin/mini/00_mini.hues.lua
+++ plugin/mini/00_mini.hues.lua
@@ -25,34 +25,37 @@ MiniDeps.now(function()
       vim.api.nvim_set_hl(0, name, data)
     end
 
-    hi("Title", { fg = p.accent, bg = nil, bold = true })
-
     -- stylua: ignore start
+    hi("Title",                          { fg = p.accent,  bg = nil,         bold = true })
+
     -- Links to Comment by default, but that has italics
-    hi("LeapBackdrop",                { link = "MiniJump2dDim" })
+    hi("LeapBackdrop",                   { link = "MiniJump2dDim" })
 
     -- I prefer italic fonts as I use fonts with beautiful italics.
     -- Some examples: Operator Mono, Berkeley Mono, PragmataPro, Radon
-    hi("Comment",                    { fg = p.fg_mid2, bg = nil,         italic = true })
-    hi("DiagnosticVirtualTextError", { fg = p.red,     bg = p.red_bg,    italic = true })
-    hi("DiagnosticVirtualTextHint",  { fg = p.cyan,    bg = p.cyan_bg,   italic = true })
-    hi("DiagnosticVirtualTextInfo",  { fg = p.blue,    bg = p.blue_bg,   italic = true })
-    hi("DiagnosticVirtualTextOk",    { fg = p.green,   bg = p.green_bg,  italic = true })
-    hi("DiagnosticVirtualTextWarn",  { fg = p.yellow,  bg = p.yellow_bg, italic = true })
+    hi("Comment",                        { fg = p.fg_mid2, bg = nil,         italic = true })
+    hi("DiagnosticVirtualTextError",     { fg = p.red,     bg = p.red_bg,    italic = true })
+    hi("DiagnosticVirtualTextHint",      { fg = p.cyan,    bg = p.cyan_bg,   italic = true })
+    hi("DiagnosticVirtualTextInfo",      { fg = p.blue,    bg = p.blue_bg,   italic = true })
+    hi("DiagnosticVirtualTextOk",        { fg = p.green,   bg = p.green_bg,  italic = true })
+    hi("DiagnosticVirtualTextWarn",      { fg = p.yellow,  bg = p.yellow_bg, italic = true })
 
     -- Highlight patterns for highlighting the whole line and hiding colon.
     -- See https://github.com/echasnovski/mini.nvim/discussions/783
-    hi("MiniHipatternsFixmeBody",  { fg = p.red })
-    hi("MiniHipatternsFixmeColon", { bg = p.red,    fg = p.red,    bold = true })
-    hi("MiniHipatternsHackBody",   { fg = p.yellow })
-    hi("MiniHipatternsHackColon",  { bg = p.yellow, fg = p.yellow, bold = true })
-    hi("MiniHipatternsNoteBody",   { fg = p.cyan })
-    hi("MiniHipatternsNoteColon",  { bg = p.cyan,   fg = p.cyan,   bold = true })
-    hi("MiniHipatternsTodoBody",   { fg = p.blue })
-    hi("MiniHipatternsTodoColon",  { bg = p.blue,   fg = p.blue,   bold = true })
+    hi("MiniHipatternsFixmeBody",        { fg = p.red })
+    hi("MiniHipatternsHackBody",         { fg = p.yellow })
+    hi("MiniHipatternsNoteBody",         { fg = p.cyan })
+    hi("MiniHipatternsTodoBody",         { fg = p.blue })
+    hi("MiniHipatternsFixmeColon",       { bg = p.red,     fg = p.red,       bold = true })
+    hi("MiniHipatternsHackColon",        { bg = p.yellow,  fg = p.yellow,    bold = true })
+    hi("MiniHipatternsNoteColon",        { bg = p.cyan,    fg = p.cyan,      bold = true })
+    hi("MiniHipatternsTodoColon",        { bg = p.blue,    fg = p.blue,      bold = true })
 
     -- Bold matches in MiniPick
-    hi("MiniPickMatchRanges",      { bg = nil, fg = p.cyan, bold = true})
+    hi("MiniPickMatchRanges",            { bg = nil,       fg = p.cyan,      bold = true})
+
+    -- Dim inactive MiniStarter elements
+    hi("MiniStarterInactive",            { link = "MiniJump2dDim" })
 
     -- Highlight patterns for deemphasizing the directory name, so the
     -- filename is more prominent. Visually, this makes it faster to
@@ -62,17 +65,17 @@ MiniDeps.now(function()
     hi("MiniStatuslineFilename",         { fg = p.fg_mid,  bg = p.accent_bg, bold = true })
     hi("MiniStatuslineFilenameModified", { fg = p.accent,  bg = p.accent_bg, bold = true })
 
-    hi("RenderMarkdownCodeBorder",    { bg = p.bg_mid })
-    hi("RenderMarkdownTableHead",     { fg = p.bg_mid2 })
-    hi("RenderMarkdownTableRow",      { fg = p.bg_mid2 })
-    hi("TreesitterContextLineNumber", { bg = p.bg_edge })
-    hi("TreesitterContextBottom",     { sp = p.bg_mid, underdotted = true})
+    hi("RenderMarkdownCodeBorder",       { bg = p.bg_mid })
+    hi("RenderMarkdownTableHead",        { fg = p.bg_mid2 })
+    hi("RenderMarkdownTableRow",         { fg = p.bg_mid2 })
+    hi("TreesitterContextLineNumber",    { bg = p.bg_edge })
+    hi("TreesitterContextBottom",        { sp = p.bg_mid,  underdotted = true})
 
     -- I like my vertical split divider to be the same color as my inactive
     -- horizontal status bar color, so it's consistent both vertically and
     -- horizontally when laststatus=2.
-    hi("VertSplit",    { fg = p.bg_edge, bg = nil })
-    hi("WinSeparator", { fg = p.bg_edge, bg = nil })
+    hi("VertSplit",                      { fg = p.bg_edge, bg = nil })
+    hi("WinSeparator",                   { fg = p.bg_edge, bg = nil })
     -- stylua: ignore end
   end
 
