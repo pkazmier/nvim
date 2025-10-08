9260df235bd0f171425e99cd30d437c8313c18a1
Pete Kazmier <pete@kazmier.com>
2025-10-08 09:25:33 -0400

    refactor(minibasics): use default options now
    Previously the options were used to specify the type of borders, but now
    we set border type via 'winborder' option.

--- plugin/mini/00_mini.basics.lua
+++ plugin/mini/00_mini.basics.lua
@@ -1,6 +1,5 @@
 MiniDeps.now(function()
   require("mini.basics").setup({
-    options = { extra_ui = false, win_borders = "bold" },
     mappings = { windows = true, move_with_alt = true },
     autocommands = { relnum_in_visual_mode = true },
   })
