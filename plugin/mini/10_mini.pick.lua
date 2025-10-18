ff41fd0381077249d53f37b76bdc624a3f7a779e
Pete Kazmier <pete@kazmier.com>
2025-10-18 10:26:29 -0400

    feat(minipick): add custom plugin picker

--- plugin/mini/10_mini.pick.lua
+++ plugin/mini/10_mini.pick.lua
@@ -19,19 +19,16 @@ MiniDeps.later(function()
 
   -- Config picker
   MiniPick.registry.config = function()
-    return MiniPick.builtin.files(nil, { source = { cwd = vim.fn.stdpath("config") } })
+    return MiniPick.builtin.files(nil, { source = { name = "Config Files", cwd = vim.fn.stdpath("config") } })
   end
 
+  -- Plugin picker
+  local plugin_dir = vim.fn.stdpath("data") .. "/site/pack/core/opt"
+  MiniPick.registry.plugins = H.two_stage_dir_picker(plugin_dir, "Plugin Picker")
+
   -- Project picker
-  MiniPick.registry.projects = function()
-    local cwd = vim.fn.expand("~/repos")
-    local choose = function(item)
-      vim.schedule(function()
-        MiniPick.builtin.files(nil, { source = { cwd = item.path } })
-      end)
-    end
-    return MiniExtra.pickers.explorer({ cwd = cwd }, { source = { choose = choose } })
-  end
+  local repo_dir = vim.fn.expand("~/repos")
+  MiniPick.registry.projects = H.two_stage_dir_picker(repo_dir, "Repo Picker")
 
   -- Aligned Grep live picker
   MiniPick.registry.grep_live_align = function(opts)
@@ -52,90 +49,109 @@ MiniDeps.later(function()
       },
     })
   end
-
-  ----------------------------------------
-  -- Helpers
-  ----------------------------------------
-
-  H.minipick_align_on_nul = function(buf_id, items, query)
-    -- Shorten the pathname to keep the width of the picker window to something
-    -- a bit more reasonable for longer pathnames.
-    items = H.map_gsub(items, "^%Z+", H.truncate_path(3))
-
-    -- Because items is an array of blobs (contains a NUL byte), align_strings
-    -- will not work because it expects strings. So, convert the NUL bytes to a
-    -- unique (hopefully) separator, then align, and revert back.
-    items = H.map_gsub(items, "%z", "#|#")
-    items = MiniAlign.align_strings(items, {
-      justify_side = { "left", "right", "right" },
-      merge_delimiter = { "", " ", "", " ", "" },
-      split_pattern = "#|#",
-    })
-    items = H.map_gsub(items, "#|#", "\0")
-
-    -- Back to the regularly scheduled program :-)
-    MiniPick.default_show(buf_id, items, query)
-  end
-
-  H.minipick_highlight_keywords = function(bufnr)
-    local ns_id = vim.api.nvim_create_namespace("kaz-keywords")
-    local keywords = {}
-    for _, keyword in ipairs({ "TODO", "FIXME", "HACK", "NOTE" }) do
-      keywords[" " .. keyword .. ":"] = H.keyword_to_hl_groups(keyword)
-    end
-
-    local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
-    local extmark_opts = { hl_mode = "combine", priority = 201 }
-
-    for row, line in ipairs(lines) do
-      for word, hl_group in pairs(keywords) do
-        local start_idx, end_idx = line:find(word)
-        if start_idx then
-          -- Highlights the keyword
-          extmark_opts.hl_group = hl_group.keyword
-          extmark_opts.end_row = row - 1
-          extmark_opts.end_col = end_idx - 1
-          vim.api.nvim_buf_set_extmark(bufnr, ns_id, row - 1, start_idx - 1, extmark_opts)
-
-          -- Highlights the ':'
-          extmark_opts.hl_group = hl_group.colon
-          extmark_opts.end_col = end_idx
-          vim.api.nvim_buf_set_extmark(bufnr, ns_id, row - 1, end_idx - 1, extmark_opts)
-
-          -- -- Highlights the rest of the line
-          extmark_opts.hl_group = hl_group.body
-          extmark_opts.end_col = #line
-          vim.api.nvim_buf_set_extmark(bufnr, ns_id, row - 1, end_idx, extmark_opts)
-        end
-      end
-    end
-  end
-
-  H.sep = package.config:sub(1, 1)
-  H.truncate_path = function(max_parts)
-    max_parts = vim.fn.max({ max_parts, 3 })
-    return function(path)
-      local parts = vim.split(path, H.sep)
-      if #parts > max_parts then
-        parts = { parts[1], "…", parts[#parts - 1], parts[#parts] }
-      end
-      return table.concat(parts, H.sep)
-    end
-  end
-
-  H.map_gsub = function(items, pattern, replacement)
-    return vim.tbl_map(function(item)
-      item, _ = string.gsub(item, pattern, replacement)
-      return item
-    end, items)
-  end
-
-  H.keyword_to_hl_groups = function(keyword)
-    keyword = keyword:sub(1, 1):upper() .. keyword:sub(2):lower()
-    return {
-      keyword = "MiniHipatterns" .. keyword,
-      colon = "MiniHipatterns" .. keyword .. "Colon",
-      body = "MiniHipatterns" .. keyword .. "Body",
-    }
-  end
 end)
+
+----------------------------------------
+-- Helpers
+----------------------------------------
+
+-- Creates a two-stage directory picker. First stage picks a directory inside
+-- `dir` with the explorer, then the second stage opens a file picker inside
+-- the chosen directory.
+H.two_stage_dir_picker = function(dir, name)
+  local pred = function(item)
+    return item.text ~= ".."
+  end
+  local choose = function(item)
+    vim.schedule(function()
+      MiniPick.builtin.files(nil, { source = { name = item.text, cwd = item.path } })
+    end)
+  end
+  return function()
+    local local_opts = { cwd = dir, filter = pred }
+    local opts = { source = { name = name, choose = choose } }
+    return MiniExtra.pickers.explorer(local_opts, opts)
+  end
+end
+
+H.minipick_align_on_nul = function(buf_id, items, query)
+  -- Shorten the pathname to keep the width of the picker window to something
+  -- a bit more reasonable for longer pathnames.
+  items = H.map_gsub(items, "^%Z+", H.truncate_path(3))
+
+  -- Because items is an array of blobs (contains a NUL byte), align_strings
+  -- will not work because it expects strings. So, convert the NUL bytes to a
+  -- unique (hopefully) separator, then align, and revert back.
+  items = H.map_gsub(items, "%z", "#|#")
+  items = MiniAlign.align_strings(items, {
+    justify_side = { "left", "right", "right" },
+    merge_delimiter = { "", " ", "", " ", "" },
+    split_pattern = "#|#",
+  })
+  items = H.map_gsub(items, "#|#", "\0")
+
+  -- Back to the regularly scheduled program :-)
+  MiniPick.default_show(buf_id, items, query)
+end
+
+H.minipick_highlight_keywords = function(bufnr)
+  local ns_id = vim.api.nvim_create_namespace("kaz-keywords")
+  local keywords = {}
+  for _, keyword in ipairs({ "TODO", "FIXME", "HACK", "NOTE" }) do
+    keywords[" " .. keyword .. ":"] = H.keyword_to_hl_groups(keyword)
+  end
+
+  local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+  local extmark_opts = { hl_mode = "combine", priority = 201 }
+
+  for row, line in ipairs(lines) do
+    for word, hl_group in pairs(keywords) do
+      local start_idx, end_idx = line:find(word)
+      if start_idx then
+        -- Highlights the keyword
+        extmark_opts.hl_group = hl_group.keyword
+        extmark_opts.end_row = row - 1
+        extmark_opts.end_col = end_idx - 1
+        vim.api.nvim_buf_set_extmark(bufnr, ns_id, row - 1, start_idx - 1, extmark_opts)
+
+        -- Highlights the ':'
+        extmark_opts.hl_group = hl_group.colon
+        extmark_opts.end_col = end_idx
+        vim.api.nvim_buf_set_extmark(bufnr, ns_id, row - 1, end_idx - 1, extmark_opts)
+
+        -- -- Highlights the rest of the line
+        extmark_opts.hl_group = hl_group.body
+        extmark_opts.end_col = #line
+        vim.api.nvim_buf_set_extmark(bufnr, ns_id, row - 1, end_idx, extmark_opts)
+      end
+    end
+  end
+end
+
+H.sep = package.config:sub(1, 1)
+H.truncate_path = function(max_parts)
+  max_parts = vim.fn.max({ max_parts, 3 })
+  return function(path)
+    local parts = vim.split(path, H.sep)
+    if #parts > max_parts then
+      parts = { parts[1], "…", parts[#parts - 1], parts[#parts] }
+    end
+    return table.concat(parts, H.sep)
+  end
+end
+
+H.map_gsub = function(items, pattern, replacement)
+  return vim.tbl_map(function(item)
+    item, _ = string.gsub(item, pattern, replacement)
+    return item
+  end, items)
+end
+
+H.keyword_to_hl_groups = function(keyword)
+  keyword = keyword:sub(1, 1):upper() .. keyword:sub(2):lower()
+  return {
+    keyword = "MiniHipatterns" .. keyword,
+    colon = "MiniHipatterns" .. keyword .. "Colon",
+    body = "MiniHipatterns" .. keyword .. "Body",
+  }
+end
