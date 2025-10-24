dc49e8951b2bf55d114fc7c2e81ad6c2a4fd8829
Pete Kazmier <pete@kazmier.com>
2025-10-24 15:25:14 -0400

    fix(copilot): manually run :Copilot auth first time

--- plugin/other/10_copilot.lua
+++ plugin/other/10_copilot.lua
@@ -3,19 +3,7 @@ MiniDeps.later(function()
     return
   end
 
-  -- Add post hook to run after first-time install
-  Config.new_autocmd({ "PackChanged" }, {
-    callback = function(ev)
-      local name, kind = ev.data.spec.name, ev.data.kind
-      if name == "copilot.lua" and kind == "install" then
-        vim.cmd.packadd("copilot.lua")
-        vim.cmd("Copilot auth")
-      end
-    end,
-  })
-
   vim.pack.add({ "https://github.com/zbirenbaum/copilot.lua" }, { load = true })
-
   require("copilot").setup({
     suggestion = {
       enabled = true,
