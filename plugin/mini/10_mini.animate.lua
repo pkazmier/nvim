39e72d1619d20a9484e379a1d09d8682f44cf09c
Pete Kazmier <pete@kazmier.com>
2025-11-01 09:50:20 -0400

    feat(minianimate): enable all animations

--- plugin/mini/10_mini.animate.lua
+++ plugin/mini/10_mini.animate.lua
@@ -1,6 +1,16 @@
 MiniDeps.later(function()
-  require("mini.animate").setup({
-    cursor = { enable = false },
-    scroll = { enable = false },
+  local animate = require("mini.animate")
+  animate.setup({
+    cursor = {
+      path = animate.gen_path.line({
+        -- Enable animation when moving horizontally within the same line as
+        -- long as the jump is more than 30 cols. By default, animation is
+        -- disabled when moving horizontally.
+        predicate = function(dest)
+          local rows, cols = unpack(dest)
+          return math.abs(rows) > 1 or math.abs(cols) > 30
+        end,
+      }),
+    },
   })
 end)
