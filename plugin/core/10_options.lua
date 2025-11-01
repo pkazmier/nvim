c47f358258f6a22d7fc402c8896d049db8bce9a6
Pete Kazmier <pete@kazmier.com>
2025-11-01 16:27:44 -0400

    feat(options): use "bold" for winborder/pumborder

--- plugin/core/10_options.lua
+++ plugin/core/10_options.lua
@@ -30,7 +30,7 @@ vim.opt.grepformat     = "%f:%l:%c:%m"
 vim.opt.grepprg        = "rg --vimgrep"
 vim.opt.list           = true
 vim.opt.listchars      = { extends="…", precedes="…", tab="  ", nbsp="␣" }
-vim.opt.pumborder      = "rounded"
+vim.opt.pumborder      = "bold"
 vim.opt.pumheight      = 10
 vim.opt.shiftround     = true
 vim.opt.shiftwidth     = 2
@@ -39,7 +39,7 @@ vim.opt.spelloptions   = "camel"
 vim.opt.splitkeep      = "cursor"
 vim.opt.textwidth      = 78
 vim.opt.wildmode       = "longest:full,full"
-vim.opt.winborder      = "rounded"
+vim.opt.winborder      = "bold"
 vim.opt.winminwidth    = 5
 
 -- ---------------------------------------------------------------------------
