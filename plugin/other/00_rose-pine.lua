33d697157cf96f64962d11b30799492841804380
Pete Kazmier <pete@kazmier.com>
2025-12-07 12:27:25 -0500

    feat(rosepine): add new theme

---
+++ plugin/other/00_rose-pine.lua
@@ -0,0 +1,45 @@
+-- ---------------------------------------------------------------------------
+-- rose pine colorscheme
+-- ---------------------------------------------------------------------------
+
+MiniDeps.now(function()
+  vim.pack.add({ { src = "https://github.com/rose-pine/neovim", name = "rose-pine" } }, { load = true })
+  require("rose-pine").setup({
+    enable = {
+      legacy_highlights = false,
+    },
+    -- stylua: ignore
+    highlight_groups = {
+      Comment                        = { fg = "muted",  italic = true },
+      MiniHipatternsFixmeBody        = { fg = "love" },
+      MiniHipatternsHackBody         = { fg = "gold" },
+      MiniHipatternsNoteBody         = { fg = "foam" },
+      MiniHipatternsTodoBody         = { fg = "iris" },
+      MiniHipatternsFixmeColon       = { bg = "love",   fg = "love", bold = true },
+      MiniHipatternsHackColon        = { bg = "gold",   fg = "gold", bold = true },
+      MiniHipatternsNoteColon        = { bg = "foam",   fg = "foam", bold = true },
+      MiniHipatternsTodoColon        = { bg = "iris",   fg = "iris", bold = true },
+      MiniMapNormal                  = { fg = "subtle" },
+      MiniPickBorderText             = { fg = "foam",   bold   = true },
+      MiniStarterInactive            = { fg = "muted",  italic = false },
+      MiniStarterSection             = { fg = "rose",   bold   = true },
+      MiniStatuslineDirectory        = { fg = "muted" },
+      MiniStatuslineFilename         = { fg = "subtle", bold   = true },
+      MiniStatuslineFilename         = { fg = "subtle", bold   = true },
+      MiniStatuslineFilenameModified = { fg = "rose",   bold   = true },
+      MiniStatuslineInactive         = { fg = "subtle", bold   = false },
+      WinSeparator                   = { fg = "overlay" },
+    },
+    palette = {
+      main = {
+        -- I think rose-pine's default pine color is simply too dark, so I use
+        -- rose-pine's moon variant of pine.
+        pine = "#3e8fb0",
+      },
+    },
+    styles = {
+      italic = false,
+    },
+  })
+  vim.cmd.colorscheme("rose-pine")
+end)
