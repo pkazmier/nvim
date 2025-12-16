9834fa09122b2d6d16f2c0067e6edcd15af49b13
Pete Kazmier <pete@kazmier.com>
2025-12-16 10:31:21 -0500

    fix(mason): load if args present

--- plugin/other/10_mason.lua
+++ plugin/other/10_mason.lua
@@ -2,7 +2,7 @@
 -- mason
 -- ---------------------------------------------------------------------------
 
-Config.later(function()
+Config.now_if_args(function()
   vim.pack.add({ "https://github.com/williamboman/mason.nvim" }, { load = true })
   require("mason").setup()
 end)
