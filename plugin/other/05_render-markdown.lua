33c7ace614180fd8c70474cc25ce85563b5fa65c
Pete Kazmier <pete@kazmier.com>
2025-12-18 15:03:18 -0500

    fix(rendermarkdown): helpers need to be defined early

--- plugin/other/05_render-markdown.lua
+++ plugin/other/05_render-markdown.lua
@@ -2,7 +2,27 @@
 -- render-markdown
 -- ---------------------------------------------------------------------------
 
-local H = {}
+local get_hl = function(hl_name)
+  local hl_info = vim.api.nvim_get_hl(0, { name = hl_name, link = false })
+  return not vim.tbl_isempty(hl_info) and hl_info or nil
+end
+
+local setup_heading_hl_groups = function()
+  local fallback_hl_info = get_hl("@markup.heading") or get_hl("Title")
+
+  for lvl = 1, 6 do
+    local hl_info = get_hl("@markup.heading." .. lvl .. ".markdown")
+      or get_hl("@markup.heading." .. lvl)
+      or fallback_hl_info
+    assert(hl_info, "Must set one of 'Title', '@markup.heading', '@markup.heading.N', or '@markup.heading.N.markdown'")
+
+    local hl_spec = { fg = hl_info.fg, bg = hl_info.bg, bold = hl_info.bold, italic = hl_info.italic }
+    vim.api.nvim_set_hl(0, "RenderMarkdownH" .. lvl, hl_spec)
+    hl_spec.reverse = true
+    vim.api.nvim_set_hl(0, "RenderMarkdownH" .. lvl .. "Bg", hl_spec)
+  end
+end
+
 Config.now_if_args(function()
   vim.pack.add({ "https://github.com/MeanderingProgrammer/render-markdown.nvim" }, { load = true })
   require("render-markdown").setup({
@@ -32,34 +52,9 @@ Config.now_if_args(function()
   })
 
   -- Set the heading hl groups as well as an autocmd for colorscheme changes.
-  H.setup_heading_hl_groups()
+  setup_heading_hl_groups()
   Config.new_autocmd("Colorscheme", {
     desc = "Setup up heading hl groups for render markdown.",
-    callback = H.setup_heading_hl_groups,
+    callback = setup_heading_hl_groups,
   })
 end)
-
--- ---------------------------------------------------------------------------
--- Helpers
--- ---------------------------------------------------------------------------
-
-H.setup_heading_hl_groups = function()
-  local fallback_hl_info = H.get_hl("@markup.heading") or H.get_hl("Title")
-
-  for lvl = 1, 6 do
-    local hl_info = H.get_hl("@markup.heading." .. lvl .. ".markdown")
-      or H.get_hl("@markup.heading." .. lvl)
-      or fallback_hl_info
-    assert(hl_info, "Must set one of 'Title', '@markup.heading', '@markup.heading.N', or '@markup.heading.N.markdown'")
-
-    local hl_spec = { fg = hl_info.fg, bg = hl_info.bg, bold = hl_info.bold, italic = hl_info.italic }
-    vim.api.nvim_set_hl(0, "RenderMarkdownH" .. lvl, hl_spec)
-    hl_spec.reverse = true
-    vim.api.nvim_set_hl(0, "RenderMarkdownH" .. lvl .. "Bg", hl_spec)
-  end
-end
-
-H.get_hl = function(hl_name)
-  local hl_info = vim.api.nvim_get_hl(0, { name = hl_name, link = false })
-  return not vim.tbl_isempty(hl_info) and hl_info or nil
-end
