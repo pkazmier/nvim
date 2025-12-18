1b1a9b034e14bb841fd897484859e324cabe4d2f
Pete Kazmier <pete@kazmier.com>
2025-12-18 06:58:31 -0500

    fix(minifiles): use hl namespace to highlight target window
    The previous implementation used 'winhighlight' option to customize the
    hl groups in the preview window. That option is a window local option,
    which has the following consequence (from docs):
    
    “The options local to a window are remembered for each buffer.”
    
    As a consequence, when a user opened a file in the explorer, our restore
    function to remove the window local option was applied to the correct
    window, but it was already hosting the new file. So when one would flip
    back to the prior buffer, the window local options were still applied.

--- plugin/mini/10_mini.files.lua
+++ plugin/mini/10_mini.files.lua
@@ -18,28 +18,32 @@ Config.later(function()
     end,
   })
 
-  Config.new_autocmd("User", {
-    desc = "Highlight target window while 'mini.files' is open",
-    pattern = "MiniFilesExplorerOpen",
-    callback = function()
-      -- Only highlight a window if there is more than one possible target
-      local n_target_win = vim
-        .iter(vim.api.nvim_tabpage_list_wins(0))
-        :filter(function(w) return vim.api.nvim_win_get_config(w).relative == "" end)
-        :fold(0, function(acc, _) return acc + 1 end)
-      if n_target_win == 1 then return end
-
-      -- Temporarily set 'winhighlight' and setup restore after closing explorer
-      local target_win_id = MiniFiles.get_explorer_state().target_window
-      local winhl_orig = vim.wo[target_win_id].winhighlight
-      vim.wo[target_win_id].winhighlight = "Normal:Visual,SignColumn:Visual"
-      local restore = function() vim.wo[target_win_id].winhighlight = winhl_orig end
-      Config.new_autocmd("User", { once = true, pattern = "MiniFilesExplorerClose", callback = restore })
-    end,
-  })
-
   Config.minifiles_open_bufdir = function()
     local path = vim.bo.buftype ~= "nofile" and vim.api.nvim_buf_get_name(0) or nil
     MiniFiles.open(path, true)
   end
+
+  -- Create hl namespace to highlight 'mini.files' target window
+  local highlight_ns = vim.api.nvim_create_namespace("highlight_minifiles_target")
+  vim.api.nvim_set_hl(highlight_ns, "Normal", { link = "Visual" })
+  vim.api.nvim_set_hl(highlight_ns, "SignColumn", { link = "Visual" })
+
+  local hl_target_win = function()
+    -- Only highlight a window if there is more than one possible target
+    local possible_targets = vim
+      .iter(vim.api.nvim_tabpage_list_wins(0))
+      :filter(function(w) return vim.api.nvim_win_get_config(w).relative == "" end)
+      :fold(0, function(acc, _) return acc + 1 end)
+    if possible_targets == 1 then return end
+
+    -- Temporarily set a hl namespace in target and setup restore after closing explorer
+    local target_win_id = MiniFiles.get_explorer_state().target_window
+    local orig_hl_ns = vim.api.nvim_get_hl_ns({ winid = target_win_id })
+    local restore = function() vim.api.nvim_win_set_hl_ns(target_win_id, orig_hl_ns ~= -1 and orig_hl_ns or 0) end
+    vim.api.nvim_win_set_hl_ns(target_win_id, highlight_ns)
+    Config.new_autocmd("User", { once = true, pattern = "MiniFilesExplorerClose", callback = restore })
+  end
+
+  local au_opts = { pattern = "MiniFilesExplorerOpen", callback = hl_target_win, desc = "Highlight target window" }
+  Config.new_autocmd("User", au_opts)
 end)
