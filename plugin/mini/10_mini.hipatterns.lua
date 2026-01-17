442648d233f796722a9e2f792a0ee3d96896ca77
Pete Kazmier <pete@kazmier.com>
2026-01-16 19:45:48 -0500

    fix(minihipatterns): account for hl-groups that use reverse
    When dynamically creating hl-groups for my custom TOOD/FIXME/HACK/etc.,
    I incorrectly assumed the bg color would be set from the hl-group I used
    as the source for the new hl-groups. Sometimes the source hl-group
    defines only a fg color with reverse=true. So I now use the bg color if
    available, but if not, then we use the fg color if reverse=true is set.

--- plugin/mini/10_mini.hipatterns.lua
+++ plugin/mini/10_mini.hipatterns.lua
@@ -2,43 +2,10 @@
 -- mini.hipatterns
 -- ---------------------------------------------------------------------------
 
+local H = {}
+
 Config.later(function()
   local hipatterns = require("mini.hipatterns")
-
-  local censor_extmark_opts = function(_, match, _)
-    local mask = string.rep("x", vim.fn.strchars(match))
-    return {
-      virt_text = { { mask, "Comment" } },
-      virt_text_pos = "overlay",
-      priority = 2000,
-      right_gravity = false,
-    }
-  end
-
-  -- Highlight patterns for highlighting the whole line and hiding colon.
-  -- See https://github.com/echasnovski/mini.nvim/discussions/783
-  --
-  -- Setup two additional hl groups to highlight the entire line while
-  -- ensureing a single space is on either side of the keyword as it's
-  -- displayed in reverse. To do this requires MiniHipatternsXXXColon and
-  -- MiniHipatternsXXXBody. We construct them based on the colors of the
-  -- builtin MiniHipatternsXXX.
-  local setup_todo_hl_groups = function()
-    for _, keyword in ipairs({ "Fixme", "Hack", "Todo", "Note" }) do
-      local name = "MiniHipatterns" .. keyword
-      local info = Config.get_hl(name) or {}
-      -- Colon group hides the ":" using same fg and bg
-      vim.api.nvim_set_hl(0, name .. "Colon", { fg = info.bg, bg = info.bg })
-      vim.api.nvim_set_hl(0, name .. "Body", { fg = info.bg })
-    end
-  end
-
-  -- Set the heading hl groups AND an autocmd for colorscheme changes.
-  setup_todo_hl_groups()
-  Config.new_autocmd("Colorscheme", {
-    desc = "Setup up todo hl groups for mini.hipatterns.",
-    callback = setup_todo_hl_groups,
-  })
 
   hipatterns.setup({
     highlighters = {
@@ -46,7 +13,7 @@ Config.later(function()
       censor = {
         pattern = "password: ()%S+()",
         group = "",
-        extmark_opts = censor_extmark_opts,
+        extmark_opts = H.censor_extmark_opts,
       },
       -- Hex colors
       hex_color = hipatterns.gen_highlighter.hex_color({
@@ -70,4 +37,40 @@ Config.later(function()
       -- stylua: ignore end
     },
   })
+
+  -- Highlight patterns for highlighting the whole line and hiding colon.
+  -- See https://github.com/echasnovski/mini.nvim/discussions/783
+  --
+  -- Setup two additional hl groups to highlight the entire line while
+  -- ensureing a single space is on either side of the keyword as it's
+  -- displayed in reverse. To do this requires MiniHipatternsXXXColon and
+  -- MiniHipatternsXXXBody. We construct them based on the colors of the
+  -- builtin MiniHipatternsXXX.
+  local setup_todo_hl_groups = function()
+    for _, keyword in ipairs({ "Fixme", "Hack", "Todo", "Note" }) do
+      local name = "MiniHipatterns" .. keyword
+      local info = Config.get_hl(name) or {}
+      local bg = info.bg or (info.reverse and info.fg)
+      -- Colon group hides the ":" using same fg and bg
+      vim.api.nvim_set_hl(0, name .. "Colon", { fg = bg, bg = bg })
+      vim.api.nvim_set_hl(0, name .. "Body", { fg = bg })
+    end
+  end
+
+  -- Set the heading todo groups AND an autocmd for colorscheme changes.
+  setup_todo_hl_groups()
+  Config.new_autocmd("Colorscheme", {
+    desc = "Setup up todo hl groups for mini.hipatterns.",
+    callback = setup_todo_hl_groups,
+  })
 end)
+
+H.censor_extmark_opts = function(_, match, _)
+  local mask = string.rep("x", vim.fn.strchars(match))
+  return {
+    virt_text = { { mask, "Comment" } },
+    virt_text_pos = "overlay",
+    priority = 2000,
+    right_gravity = false,
+  }
+end
