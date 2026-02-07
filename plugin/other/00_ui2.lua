16dc47229812160c16d109acb77810fcba2d6764
Pete Kazmier <pete@kazmier.com>
2026-02-07 11:56:10 -0500

    fix(extui): neovim changed '_extui' to '_core.ui2'

--- plugin/other/00_ui2.lua
+++ plugin/other/00_ui2.lua
@@ -1,10 +1,10 @@
 -- ---------------------------------------------------------------------------
--- extui
+-- _core.ui2
 -- ---------------------------------------------------------------------------
 
--- Enable the experimental 'extui' from neovim-0.12+
+-- Enable the experimental 'ui2' from neovim-0.12+
 --
 -- Provides syntax highlighting in command line as well as a buffer
 -- for messages that are shown. You enter the message buffer with 'g<'.
 
-Config.now(function() require("vim._extui").enable({}) end)
+Config.now(function() require("vim._core.ui2").enable({}) end)
