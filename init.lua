59d27d2225586256dd8c6b34f853ea56288c15b6
Pete Kazmier <pete@kazmier.com>
2026-02-21 15:34:42 -0500

    refactor: use new mini.misc.safely() for lazy loading

--- init.lua
+++ init.lua
@@ -5,13 +5,24 @@
 -- Bootstrap with mini
 vim.pack.add({ "https://github.com/nvim-mini/mini.nvim" })
 
--- Setup 'mini.deps' for access to `now` and `later` helpers
-require("mini.deps").setup()
-
 -- Define global config table for sharing between modules
 _G.Config = {}
 
--- Define lazy helpers
-Config.now = MiniDeps.now
-Config.now_if_args = vim.fn.argc(-1) > 0 and MiniDeps.now or MiniDeps.later
-Config.later = MiniDeps.later
+-- Setup 'mini.misc' for access to `MiniMisc.safely()`
+local misc = require("mini.misc")
+
+-- Define package helpers
+Config.now = function(f) misc.safely("now", f) end
+Config.later = function(f) misc.safely("later", f) end
+Config.now_if_args = vim.fn.argc(-1) > 0 and Config.now or Config.later
+Config.on_event = function(ev, f) misc.safely("event:" .. ev, f) end
+Config.on_filetype = function(ft, f) misc.safely("filetype:" .. ft, f) end
+Config.on_packchanged = function(plugin_name, kinds, callback, desc)
+  local f = function(ev)
+    local name, kind = ev.data.spec.name, ev.data.kind
+    if not (name == plugin_name and vim.tbl_contains(kinds, kind)) then return end
+    if not ev.data.active then vim.cmd.packadd(plugin_name) end
+    callback()
+  end
+  Config.new_autocmd("PackChanged", { pattern = "*", callback = f, desc = desc })
+end
--- plugin/core/12_functions.lua
+++ plugin/core/12_functions.lua
@@ -24,20 +24,6 @@ Config.get_hl = function(hl_name)
 end
 
 -- ---------------------------------------------------------------------------
--- Custom `vim.pack.add()` hook helper
--- ---------------------------------------------------------------------------
-
-Config.on_packchanged = function(plugin_name, kinds, callback, desc)
-  local f = function(ev)
-    local name, kind = ev.data.spec.name, ev.data.kind
-    if not (name == plugin_name and vim.tbl_contains(kinds, kind)) then return end
-    if not ev.data.active then vim.cmd.packadd(plugin_name) end
-    callback()
-  end
-  Config.new_autocmd("PackChanged", { pattern = "*", callback = f, desc = desc })
-end
-
--- ---------------------------------------------------------------------------
 -- Scratch Buffer
 -- ---------------------------------------------------------------------------
 
