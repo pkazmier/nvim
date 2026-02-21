04ccea040c5e923374c0236e9bb44c9debffefb6
Pete Kazmier <pete@kazmier.com>
2026-02-21 15:35:53 -0500

    refactor: use lua syntax for vim.opt

--- plugin/core/10_options.lua
+++ plugin/core/10_options.lua
@@ -11,46 +11,46 @@ Config.copilot_disable = vim.fn.expand("$USER") == "kaz"
 -- ---------------------------------------------------------------------------
 
 vim.g.mapleader        = " "
-vim.g.maplocalleader   = " "
 
 -- ---------------------------------------------------------------------------
 -- General settings
 -- ---------------------------------------------------------------------------
 
-vim.opt.breakindentopt = "list:-1"
-vim.opt.cmdheight      = 0
-vim.opt.complete       = '.,w,b,kspell'
-vim.opt.completeopt    = "menuone,noselect,fuzzy"
-vim.opt.conceallevel   = 2
-vim.opt.confirm        = true
-vim.opt.expandtab      = true
-vim.opt.fillchars      = { fold="╌", diff="╱", eob=" " }
-vim.opt.formatlistpat  = [[^\s*[0-9\-\+\*]\+[\.\)]*\s\+]]
-vim.opt.formatoptions  = "jcrql1nt"
-vim.opt.grepformat     = "%f:%l:%c:%m"
-vim.opt.grepprg        = "rg --vimgrep"
-vim.opt.list           = true
-vim.opt.listchars      = { extends="…", precedes="…", tab="  ", nbsp="␣" }
-vim.opt.pumborder      = "rounded"
-vim.opt.pumheight      = 10
-vim.opt.shiftround     = true
-vim.opt.shiftwidth     = 2
-vim.opt.shortmess      = "FOSWICaco"
-vim.opt.spelloptions   = "camel"
-vim.opt.splitkeep      = "cursor"
-vim.opt.textwidth      = 78
-vim.opt.winborder      = "rounded"
-vim.opt.winminwidth    = 5
+vim.opt.breakindentopt  = "list:-1"
+vim.opt.cmdheight       = 0
+vim.opt.complete        = { ".", "w", "b", "kspell" }
+vim.opt.completeopt     = { "menuone", "noselect", "fuzzy" }
+vim.opt.completetimeout = 100
+vim.opt.conceallevel    = 2
+vim.opt.confirm         = true
+vim.opt.expandtab       = true
+vim.opt.fillchars       = { fold="╌", diff="╱", eob=" " }
+vim.opt.formatlistpat   = [[^\s*[0-9\-\+\*]\+[\.\)]*\s\+]]
+vim.opt.formatoptions   = "jcrql1nt"
+vim.opt.grepformat      = "%f:%l:%c:%m"
+vim.opt.grepprg         = "rg --vimgrep"
+vim.opt.list            = true
+vim.opt.listchars       = { extends="…", precedes="…", tab="  ", nbsp="␣" }
+vim.opt.pumborder       = "rounded"
+vim.opt.pumheight       = 10
+vim.opt.shiftround      = true
+vim.opt.shiftwidth      = 2
+vim.opt.shortmess       = "FOSWICaco"
+vim.opt.spelloptions    = "camel"
+vim.opt.splitkeep       = "cursor"
+vim.opt.textwidth       = 78
+vim.opt.winborder       = "rounded"
+vim.opt.winminwidth     = 5
 
 -- ---------------------------------------------------------------------------
 -- Fold settings
 -- ---------------------------------------------------------------------------
 
-vim.opt.foldexpr       = "v:lua.vim.treesitter.foldexpr()"
-vim.opt.foldlevel      = 99
-vim.opt.foldmethod     = "expr"
-vim.opt.foldnestmax    = 10
-vim.opt.foldtext       = ""
+vim.opt.foldexpr    = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldlevel   = 99
+vim.opt.foldmethod  = "expr"
+vim.opt.foldnestmax = 10
+vim.opt.foldtext    = ""
 
 -- ---------------------------------------------------------------------------
 -- Diagnostics
