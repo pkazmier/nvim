8f9e469c3e7adff3f60037fe7ea3a468b5a60c2b
Pete Kazmier <pete@kazmier.com>
2026-02-21 15:35:27 -0500

    fix: use now_if_args for mini.completion

--- plugin/mini/10_mini.completion.lua
+++ plugin/mini/10_mini.completion.lua
@@ -2,14 +2,12 @@
 -- mini.completion
 -- ---------------------------------------------------------------------------
 
-Config.later(function()
+Config.now_if_args(function()
   require("mini.completion").setup({
     lsp_completion = { source_func = "omnifunc", auto_setup = false },
     mappings = { force_fallback = "" },
   })
-  local on_attach = function(args)
-    vim.bo[args.buf].omnifunc = "v:lua.MiniCompletion.completefunc_lsp"
-  end
+  local on_attach = function(args) vim.bo[args.buf].omnifunc = "v:lua.MiniCompletion.completefunc_lsp" end
   Config.new_autocmd("LspAttach", { callback = on_attach })
 
   -- Advertise to servers that Neovim now supports certain set of completion and
