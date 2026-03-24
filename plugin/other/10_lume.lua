09e6eaec9416141ad7801159f27909deffeed532
Pete Kazmier <pete@kazmier.com>
2026-03-24 12:33:08 -0400

    fix(lume): add color to @properties

--- plugin/other/10_lume.lua
+++ plugin/other/10_lume.lua
@@ -4,5 +4,13 @@
 
 Config.later(function()
   vim.pack.add({ { src = "https://github.com/danfry1/lume" } })
-  require("catppuccin").setup({})
+  require("lume").setup({
+    custom_highlights = function(colors, variant)
+      return {
+        -- Lume doesn't add color to properties, which is bland IMO.
+        ["@property"] = { fg = colors.accents.teal },
+        ["@lsp.type.variable"] = { link = "@lsp" },
+      }
+    end,
+  })
 end)
