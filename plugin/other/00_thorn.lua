6245176adc9da3a1c2295bdb45a01c9d1d32b7fc
Pete Kazmier <pete@kazmier.com>
2026-05-18 10:39:17 -0400

    fix(thorn): markup and render-markdown improvements
    I like bold and italic to also use a different color to help distinguish
    them from normal text. In addition, tuned the render-markdown table
    borders.

--- plugin/other/00_thorn.lua
+++ plugin/other/00_thorn.lua
@@ -9,6 +9,10 @@ Config.now(function()
       hl.LeapLabel = { fg = p.bg, bg = p.orange, bold = true }
       hl.MiniStatuslineDirectory = { bg = p.statusbar.bg, fg = p.gray }
       hl.MiniStatuslineFilename = { bg = p.statusbar.bg, fg = p.gray, bold = true }
+      hl.RenderMarkdownTableHead = { fg = p.green_5 }
+      hl.RenderMarkdownTableRow = { fg = p.green_5 }
+      hl["@markup.strong"] = { fg = p.green_2, bold = true }
+      hl["@markup.italic"] = { fg = p.green_2, italic = true }
     end,
   })
   vim.cmd.colorscheme("thorn")
